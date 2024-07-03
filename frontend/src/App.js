b352e0d5b5c613053c7333331306ce53d6189abc
BassamHafez <bassamhafez790@gmail.com>
2024-07-03 19:15:36 +0300

    initialize setup for  project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./Pages/Root";
+import Home from './Pages/Home/Home';
+import SpecialCards from './Pages/SpecialCards/SpecialCards';
 
 function App() {
+
+  const router=createBrowserRouter([{
+    path:"/",
+    element:<Root/>,
+    children:[
+      {index:true,element:<Home/>},
+      {path:"special-cards",element:<SpecialCards/>}
+    ]
+  }])  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router}/>
+    </>
   );
 }
 
