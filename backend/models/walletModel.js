28133aafcac25d5c49dcddabfa4922abf796f244
Ammar <ammar.yassr.33@gmail.com>
2024-07-06 20:53:19 +0300

    Create wallet model

---
+++ backend/models/walletModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const walletSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Wallet must belong to a user"],
+  },
+  balance: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Wallet = mongoose.model("Wallet", walletSchema);
+
+module.exports = Wallet;
