311c66efc03f7235ea946171dcd2153949b53861
Ammar <ammar.yassr.33@gmail.com>
2024-09-06 14:04:14 +0300

    Refactor shop data in sampleData.js

--- backend/models/sampleData.js
+++ backend/models/sampleData.js
@@ -65,70 +65,92 @@ const uploadSampleData = async () => {
         {
           name: "Elct",
           logo: "shop1.png",
+          description: "Short description for the shop",
         },
         {
           name: "DURMA",
           logo: "shop2.png",
+          description: "Short description for the shop",
         },
         {
           name: "CROWD",
           logo: "shop3.png",
+          description: "Short description for the shop",
         },
         {
           name: "MOUEN",
           logo: "shop4.png",
+          description: "Short description for the shop",
         },
         {
           name: "THE POP UP",
           logo: "shop5.png",
+          description: "Short description for the shop",
+        },
+        {
+          name: "VEO",
+          logo: "shop6.jpg",
+          description: "Short description for the shop",
         },
         {
           name: "فورج",
-          logo: "shop7.jpg",
+          logo: "shop7.png",
+          description: "Short description for the shop",
         },
         {
           name: "مهاب",
           logo: "shop8.jpg",
+          description: "Short description for the shop",
         },
         {
           name: "Feel Good Tea",
           logo: "shop9.jpg",
+          description: "Short description for the shop",
         },
         {
           name: "GREEN GARDEN",
           logo: "shop10.jpg",
+          description: "Short description for the shop",
         },
         {
           name: "دايرتين",
-          logo: "shop11.jpg",
+          logo: "shop11.png",
+          description: "Short description for the shop",
         },
         {
           name: "NOYA",
           logo: "shop12.jpeg",
+          description: "Short description for the shop",
         },
         {
           name: "2 GETHER",
           logo: "shop13.jpeg",
+          description: "Short description for the shop",
         },
         {
           name: "FUN VIBES",
           logo: "shop13.png",
+          description: "Short description for the shop",
         },
         {
           name: "شاهين",
-          logo: "shop114.png",
+          logo: "shop14.png",
+          description: "Short description for the shop",
         },
         {
           name: "RUMORS",
           logo: "shop15.png",
+          description: "Short description for the shop",
         },
         {
           name: "نجد wear",
           logo: "shop16.png",
+          description: "Short description for the shop",
         },
         {
           name: "4TWINS",
           logo: "shop17.png",
+          description: "Short description for the shop",
         },
       ]),
     ]);
