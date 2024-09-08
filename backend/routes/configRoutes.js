454dd4d8c9d51b1a77ffdc95d86c4b04cb785ac2
Ammar <ammar.yassr.33@gmail.com>
2024-09-08 19:05:04 +0300

    Refactor configRoutes to allow getConfigs for all

--- backend/routes/configRoutes.js
+++ backend/routes/configRoutes.js
@@ -4,9 +4,10 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const configController = require("../controllers/configController");
 
+router.get("/", configController.getConfigs);
+
 router.use(authController.protect, authController.restrictTo("admin"));
 
-router.get("/", configController.getConfigs);
 router.patch("/", configController.updateConfigs);
 
 module.exports = router;
