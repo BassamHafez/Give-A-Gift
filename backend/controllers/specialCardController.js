f2bb788e23cebf2b6b5d15cac470912b142a9ad5
Ammar <ammar.yassr.33@gmail.com>
2024-09-19 21:22:57 +0300

    Refactor specialCardController to use image paths for front and back shapes

--- backend/controllers/specialCardController.js
+++ backend/controllers/specialCardController.js
@@ -1,7 +1,6 @@
 const { v4: uuidv4 } = require("uuid");
 const sharp = require("sharp");
 const SpecialCard = require("../models/SpecialCardModel");
-const Shape = require("../models/shapeModel");
 const Config = require("../models/configModel");
 const factory = require("./handlerFactory");
 const catchAsync = require("../utils/catchAsync");
@@ -12,29 +11,18 @@ exports.updateCard = factory.updateOne(SpecialCard);
 exports.deleteCard = factory.deleteOne(SpecialCard);
 
 exports.getAllCards = catchAsync(async (req, res, next) => {
-  const [cards, frontShape, backShape] = await Promise.all([
+  const [cards, frontShapeImagePath, backShapeImagePath] = await Promise.all([
     SpecialCard.find().populate({ path: "shop", select: "name logo" }),
-    Config.findOne({ key: "SPECIAL_FRONT_SHAPE_ID" }),
-    Config.findOne({ key: "SPECIAL_BACK_SHAPE_ID" }),
+    Config.findOne({ key: "SPECIAL_FRONT_SHAPE_PATH" }),
+    Config.findOne({ key: "SPECIAL_BACK_SHAPE_PATH" }),
   ]);
-
-  console.log("frontShape :>> ", frontShape);
-  console.log("backShape :>> ", backShape);
-
-  const [frontShapeData, backShapeData] = await Promise.all([
-    Shape.findById(frontShape.value),
-    Shape.findById(backShape.value),
-  ]);
-
-  console.log("frontShapeData :>> ", frontShapeData);
-  console.log("backShapeData :>> ", backShapeData);
 
   res.status(200).json({
     status: "success",
     results: cards.length,
     data: {
-      frontShape: frontShapeData.image,
-      backShape: backShapeData.image,
+      frontShapeImagePath: frontShapeImagePath?.value,
+      backShapeImagePath: backShapeImagePath?.value,
       cards,
     },
   });
