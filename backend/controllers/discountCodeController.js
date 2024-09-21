c79866b8bf1ef06979f1d0abdafb4c3c935b695b
Ammar <ammar.yassr.33@gmail.com>
2024-09-21 21:04:22 +0300

    Refactor getAllCodes to use aggregation pipeline

--- backend/controllers/discountCodeController.js
+++ backend/controllers/discountCodeController.js
@@ -1,24 +1,36 @@
 const User = require("../models/userModel");
 const Card = require("../models/cardModel");
 const catchAsync = require("../utils/catchAsync");
+const ApiError = require("../utils/ApiError");
 
 exports.getAllDiscountCodes = catchAsync(async (req, res, next) => {
   const merchant = await User.findById(req.user.id);
 
-  const cards = await Card.find({ shop: merchant.merchantShop });
+  if (!merchant || !merchant.merchantShop) {
+    return next(new ApiError("You do not have an associated shop", 404));
+  }
 
-  const discountCodes = cards.map((card) => {
-    return {
-      id: card._id,
-      recipient: card.recipient.name,
-      isUsed: card.discountCode.isUsed,
-      usedAt: card.discountCode.usedAt,
-      isPaid: card.isPaid,
-    };
-  });
+  const discountCodes = await Card.aggregate([
+    {
+      $match: {
+        shop: merchant.merchantShop,
+      },
+    },
+    {
+      $project: {
+        id: "$_id",
+        _id: 0,
+        recipient: "$recipient.name",
+        isUsed: "$discountCode.isUsed",
+        usedAt: "$discountCode.usedAt",
+        isPaid: 1,
+      },
+    },
+  ]);
 
   res.status(200).json({
     status: "success",
+    results: discountCodes.length,
     data: discountCodes,
   });
 });
