fdf96583bb8466fe4c7fde573632e313a9436f8d
BassamHafez <bassamhafez790@gmail.com>
2024-09-23 19:51:12 +0300

    removing replacing  fonts

--- frontend/src/Components/Logic/Logic.js
+++ frontend/src/Components/Logic/Logic.js
@@ -6,21 +6,21 @@ import usa from "../../Images/countries/usa.png";
 import uae from "../../Images/countries/uae.png";
 
 export const FontsFamilies = [
-    { label: "'ARAHAMAH1982', sans-serif", value: "'ARAHAMAH1982', sans-serif" },
-    { label: "Arial, Helvetica, sans-serif", value: "Arial, Helvetica, sans-serif" },
+    { label: "ARAHAMAH1982", value: "ARAHAMAH1982" },
+    { label: "Arial, Helvetica", value: "Arial, Helvetica" },
     { label: "'Times New Roman', Times, serif", value: "'Times New Roman', Times, serif" },
-    { label: "Consolas, 'Courier New', monospace", value: "Consolas, 'Courier New', monospace" },
-    { label: "'Courier New', Courier, monospace", value: "'Courier New', Courier, monospace" },
-    { label: "'Lucida Console', Monaco, monospace", value: "'Lucida Console', Monaco, monospace" },
-    { label: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif", value: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif" },
-    { label: "'Cascadia Code', Menlo, monospace", value: "'Cascadia Code', Menlo, monospace" },
-    { label: "'Fira Code', Menlo, monospace", value: "'Fira Code', Menlo, monospace" },
-    { label: "'Source Code Pro', Menlo, monospace", value: "'Source Code Pro', Menlo, monospace" },
-    { label: "'JetBrains Mono', Menlo, monospace", value: "'JetBrains Mono', Menlo, monospace" },
-    { label: "'Roboto Mono', Menlo, monospace", value: "'Roboto Mono', Menlo, monospace" },
-    { label: "'Monaco', Menlo, monospace", value: "'Monaco', Menlo, monospace" },
-    { label: "'Open Sans', sans-serif", value: "'Open Sans', sans-serif" },
-    { label: "'Noto Sans', sans-serif", value: "'Noto Sans', sans-serif" },
+    { label: "Consolas, 'Courier New'", value: "Consolas, 'Courier New'" },
+    { label: "'Courier New', Courier", value: "'Courier New', Courier" },
+    { label: "'Lucida Console', Monaco", value: "'Lucida Console', Monaco" },
+    { label: "'Segoe UI', Tahoma, Geneva, Verdana", value: "'Segoe UI', Tahoma, Geneva, Verdana" },
+    { label: "'Cascadia Code', Menlo", value: "'Cascadia Code', Menlo" },
+    { label: "'Fira Code', Menlo", value: "'Fira Code', Menlo" },
+    { label: "'Source Code Pro', Menlo", value: "'Source Code Pro', Menlo" },
+    { label: "'JetBrains Mono', Menlo", value: "'JetBrains Mono', Menlo" },
+    { label: "'Roboto Mono', Menlo", value: "'Roboto Mono', Menlo" },
+    { label: "'Monaco', Menlo", value: "'Monaco', Menlo" },
+    { label: "Open Sans", value: "Open Sans" },
+    { label: "Noto Sans", value: "Noto Sans" },
   ];
   
 
