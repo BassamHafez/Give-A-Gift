15bfaad995a5485073fb5c083d5ee3bdd919e480
BassamHafez <bassamhafez790@gmail.com>
2024-09-28 19:22:49 +0300

    scroll top on orders

--- frontend/src/Pages/Admin/AdminPages/Orders/Orders.jsx
+++ frontend/src/Pages/Admin/AdminPages/Orders/Orders.jsx
@@ -3,9 +3,10 @@ import styles from "./Orders.module.css";
 import OrdersDataView from "./OrdersDataView";
 
 const Orders = () => {
-  useEffect(()=>{
-    window.scrollTo(0, 0)
-  },[])
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
