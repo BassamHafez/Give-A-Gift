b7639ccb01dcf6234fa72d4ccc19f8eac6cf27cd
Ammar <ammar.yassr.33@gmail.com>
2024-10-20 14:58:30 +0300

    Refactor adModel schema to include timestamps

--- backend/models/adModel.js
+++ backend/models/adModel.js
@@ -1,23 +1,26 @@
 const mongoose = require("mongoose");
 
-const adSchema = new mongoose.Schema({
-  image: {
-    type: String,
-    required: true,
+const adSchema = new mongoose.Schema(
+  {
+    image: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    order: {
+      type: Number,
+      required: true,
+    },
+    size: {
+      type: String,
+      enum: ["small", "large"],
+    },
   },
-  link: {
-    type: String,
-    required: true,
-  },
-  order: {
-    type: Number,
-    required: true,
-  },
-  size: {
-    type: String,
-    enum: ["small", "large"],
-  },
-});
+  { timestamps: true }
+);
 
 const Ad = mongoose.model("Ad", adSchema);
 
