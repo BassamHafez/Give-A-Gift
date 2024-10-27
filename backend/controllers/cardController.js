a0c4f3018a61d97b1ca4f437e4be32d310151ec1
Ammar <ammar.yassr.33@gmail.com>
2024-10-27 23:00:28 +0300

    Refactor cardPopulateOptions to include 'cardLogo' for shop

--- backend/controllers/cardController.js
+++ backend/controllers/cardController.js
@@ -20,7 +20,7 @@ exports.filterUserCards = (req, res, next) => {
 
 const cardPopulateOptions = [
   { path: "user", select: "name phone" },
-  { path: "shop", select: "name logo link" },
+  { path: "shop", select: "name logo cardLogo link" },
   { path: "shapes.shape", select: "image price" },
   { path: "color", select: "hex" },
   { path: "proColor", select: "image price" },
